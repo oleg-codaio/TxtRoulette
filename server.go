70a26a5715c721dc9c2e2720ce4c1ac2aa8db204
Oleg Vaskevich <vaskevich.o@husky.neu.edu>
2015-03-28 18:07:56 -0400

    Check in working code

--- server.go
+++ server.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	//"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	//"strings"
+)
+
+func receive(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
 
 func main() {
-	fmt.Println("Hello, 世界")
+	// Read the port from the file.
+	//fileContents, err := ioutil.ReadFile("port.txt")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//port := strings.TrimSpace(string(fileContents))
+	if len(os.Args) != 2 {
+		log.Fatal("usage: server.go port")
+	}
+	port := os.Args[1]
+
+	// Start the server.
+	fmt.Printf("Starting TxtRoulette server on port %s...\n", port)
+	http.HandleFunc("/receive", receive)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
