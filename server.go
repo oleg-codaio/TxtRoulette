d01b1d44dc950201c9591ab466db2cd7476bafcc
Kenneth Smith <smith.kenny11@gmail.com>
2015-03-28 22:26:52 -0400

    added sendSMS to send a text message to a phone number

--- server.go
+++ server.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	//"io"
-	//"io/ioutil"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	//"strings"
+	"net/url"
+	"strings"
 )
 
 func receive(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +18,43 @@ func receive(w http.ResponseWriter, r *http.Request) {
 	phone := r.URL.Query()["From"]
 
 	fmt.Printf("wholeurl:\n%s\n\nPhone: %s\nBody: %s,\n\n", wholeurl, phone, body)
+}
+
+func sendSMS(phonenumber , message string) {
+	
+	apiusr := os.Getenv("TWILIO_APIUSR")
+	apikey := os.Getenv("TWILIO_APIKEY")
+	
+	u := "https://api.twilio.com/2010-04-01/Accounts/AC7dbbd979132aeb252095fa79059a5de4/Messages.json"
+	
+	hc := http.Client{}
+	form := url.Values{}
+	form.Add("To", phonenumber)
+	form.Add("From", "+13208398785")
+	form.Add("Body", message)
+
+	req, err := http.NewRequest("POST", u, strings.NewReader(form.Encode()))
+        
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.SetBasicAuth(apiusr, apikey)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	//fmt.Printf("the request was: \n%v\n\n",req)
+
+	resp, err := hc.Do(req)
+        if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != 201 {
+		defer resp.Body.Close()
+		body, err :=ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print(body)
+	}
 }
 
 func main() {
@@ -32,6 +70,9 @@ func main() {
 	}
 
 	port := ":" + os.Args[1]
+
+	//fmt.Printf("sending text message\n")
+	//sendSMS("+17744024454", "Test message from Twilio")
 
 	// Start the server.
 	fmt.Printf("Starting TxtRoulette server on port %s...\n", port)
